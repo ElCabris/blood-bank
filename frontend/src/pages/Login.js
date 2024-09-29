008798d90ae5e37a06b41c6d690970bfb32da2ec
Emanuel Cabrera Novoa <133041097+ElCabris@users.noreply.github.com>
2024-09-28 23:26:35 -0500

    fix: Fixed bug where tabs was not displayed (#26)
    The content of the pages `login`, `registrar donante`, ​​`regi` was not displayed
    - Login.js: the `return` instruction is added
    - App.js: The routes are corrected:
            - register-bank to register/bank
            - register-donor to register/donor

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,15 @@
-import InputData from "../components/InputData"; 
+import InputData from "../components/InputData";
 function Login() {
-	<>
+	return (
+		<>
 		<h1>Iniciar Sesión</h1>
-	<form>
-		<InputData>Usuario</InputData>
-		<InputData>Contraseña</InputData>
-	</form>
+		<form>
+			<InputData>Usuario</InputData>
+			<InputData>Contraseña</InputData>
+		</form>
 		<button>Enviar</button>
 	</>
+	);
 }
 
 export default Login;
