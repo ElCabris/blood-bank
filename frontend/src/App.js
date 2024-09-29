fa1ec93805270f354260f423c65acc8ab165640c
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-09-28 19:15:52 -0500

    feat: Donor Home added.
    - Connects to: "Donation Sites" and "Donation History"
    - Inbox and Edit Profile left to do.
    
    - App.js: Removed routes for donation sites and donation history
    - DonorEditProfile.js: File to add menu to edit donor profile
    - DonorHome.js: Donor Home component
    - MenuDonor.js: Donor selection component
    
    - resolves #12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,52 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
 import RegisterBank from "./pages/RegisterBank";
 import RegisterDonor from "./pages/RegisterDonor";
 import DonationSites from "./pages/DonationSites";
 import DonationHistory from "./pages/DonationHistory";
 import Login from "./pages/Login";
 import Home from './pages/Home';
+import DonorHome from './pages/DonorHome';
 
 function App() {
-  return (
-    <Router>
-      <nav className='navbar'>
-        <ul> 
-          <li>
-            <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>
-              Casa
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>
-              Iniciar Sesión
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/register-bank" className={({ isActive }) => isActive ? 'active' : ''}>
-              Registrar Banco
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/register-donor" className={({ isActive }) => isActive ? 'active' : ''}>
-              Registrar Donante
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+	return (
+		<Router>
+			<nav className='navbar'>
+				<ul>
+					<li>
+						<NavLink to="/" className={({isActive}) => isActive ? 'active' : ''}>
+							Casa
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/login" className={({isActive}) => isActive ? 'active' : ''}>
+							Iniciar Sesión
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/register-bank" className={({isActive}) => isActive ? 'active' : ''}>
+							Registrar Banco
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/register-donor" className={({isActive}) => isActive ? 'active' : ''}>
+							Registrar Donante
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
 
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register-bank" element={<RegisterBank />} />
-          <Route path="/register-donor" element={<RegisterDonor />} />
-          <Route path="/donation-sites" element={<DonationSites />} />
-          <Route path="/donation-history" element={<DonationHistory />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+			<div className="main-content">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/donor" element={<DonorHome />} />
+					<Route path="/register/bank" element={<RegisterBank />} />
+					<Route path="/register/donor" element={<RegisterDonor />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
