bdcd2700e796740acdf45c40b49c2ad28b45dfdf
Emanuel Cabrera Novoa <133041097+ElCabris@users.noreply.github.com>
2024-09-30 04:01:24 -0500

    feat: vertical format for homes (#46)

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,7 +1,7 @@
 function MenuDonor({setActivateComponent}) {
 	return (
 		<nav>
-			<ul>
+			<ul style={{display: 'flex', gap: '20px', listStyle: 'none', padding: 0}}>
 				<li>
 					<button onClick={() => setActivateComponent('edit-profile')}>
 						Modificar Perfil
@@ -23,48 +23,48 @@ function MenuDonor({setActivateComponent}) {
 	);
 }
 
-export function MenuBank({setActivateComponent}) {
-	return (
-		<nav>
-			<ul>
-				<li>
-					<button onClick={() => setActivateComponent('edit-profile')}>
-						Modificar Perfil
-					</button>
-				</li>
-				<li>
-					<button onClick={() => setActivateComponent('chat')}>
-						Chat
-					</button>
-				</li>
-				<li>
-					<button onClick={() => setActivateComponent('request-donor')}>
-						Solicitar donante
-					</button>
-				</li>
-				<li>
-					Almacenamiento
-					<ul>
-						<li>
-							<button onClick={() => setActivateComponent('statistics')}>
-								Ver Estadísticas
-							</button>
-						</li>
-						<li>
-							<button onClick={() => setActivateComponent('add batch')}>
-								Agregar Lote
-							</button>
-						</li>
-						<li>
-							<button onClick={() => setActivateComponent('delete batch')}>
-								Eliminar Lote
-							</button>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</nav>
-	);
+export function MenuBank({ setActivateComponent }) {
+  return (
+    <nav>
+      <ul style={{ display: 'flex', gap: '20px', listStyle: 'none', padding: 0 }}>
+        <li>
+          <button onClick={() => setActivateComponent('edit-profile')}>
+            Modificar Perfil
+          </button>
+        </li>
+        <li>
+          <button onClick={() => setActivateComponent('chat')}>
+            Chat
+          </button>
+        </li>
+        <li>
+          <button onClick={() => setActivateComponent('request-donor')}>
+            Solicitar donante
+          </button>
+        </li>
+        <li>
+          Almacenamiento
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            <li>
+              <button onClick={() => setActivateComponent('statistics')}>
+                Ver Estadísticas
+              </button>
+            </li>
+            <li>
+              <button onClick={() => setActivateComponent('add batch')}>
+                Agregar Lote
+              </button>
+            </li>
+            <li>
+              <button onClick={() => setActivateComponent('delete batch')}>
+                Eliminar Lote
+              </button>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default MenuDonor;
