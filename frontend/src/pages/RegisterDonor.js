42429890df918d3c0b1c44bc79d40248a0f1c635
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-09-30 01:15:34 -0500

    feat: Labels for entry types when registering a donor
    - added types for each input
    - removed the div tag from the InputData component, in order to make it more adaptable to the context in which it is used
    
    - solve #29

--- frontend/src/pages/RegisterDonor.js
+++ frontend/src/pages/RegisterDonor.js
@@ -5,17 +5,21 @@ function RegisterDonor() {
 			<InputData>Nombres</InputData>
 			<InputData>Apellidos</InputData>
 			<InputData>Fecha de nacimiento</InputData>
-			<InputData>Género</InputData>
-			<InputData>Teléfono</InputData>
-			<InputData>Correo electrónico</InputData>
-			<InputData>Historial médico</InputData>
+		<select name="gener">
+		<option>Hombre</option>
+		<option>Mujer</option>
+		<option>Elicoptero de batalla</option>
+		</select>
+			<InputData type="number">Teléfono</InputData>
+			<InputData type="email">Correo electrónico</InputData>
+			<InputData type="file">Historial médico*</InputData>
 			<InputData>Grupo sanguíneo</InputData>
 			<InputData>Ciudad / Municipio</InputData>
 			<InputData>Dirección</InputData>
 			<InputData>Usuario</InputData>
-			<InputData>Contraseña</InputData>
-			<InputData>Confirma contraseña</InputData>
-			<button>Enviar</button>
+			<InputData type="password">Contraseña</InputData>
+			<InputData type="password">Confirma contraseña</InputData>
+			<button type="submit">Enviar</button>
 		</form>
 	);
 }
