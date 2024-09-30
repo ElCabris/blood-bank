5c82f2030ed16649fcad161e32cdc77518d3e93a
Emanuel Cabrera Novoa <133041097+ElCabris@users.noreply.github.com>
2024-09-30 01:20:55 -0500

    feat: Labels for entry types when registering a donor (#41)
    - added types for each input
    - removed the div tag from the InputData component, in order to make it more adaptable to the context in which it is used
    
    - solve #29

--- frontend/src/components/InputData.js
+++ frontend/src/components/InputData.js
@@ -1,9 +1,9 @@
 function InputData({children, type = 'text', ...rest}) {
 	return (
-		<div>
+		<>
 			<label htmlFor={children}>{children}</label>
 			<input {...rest} id={children} type={type}/>
-		</div>
+		</>
 	)
 }
 
