88b00a94eaf147387248c1f7932ed76905014317
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-10-24 15:53:39 -0500

    feat: db_session decorator for database

--- backend/app/db/data.py
+++ backend/app/db/data.py
@@ -1,8 +1,25 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 URL_DATA = "sqlite:///./data.db"
 engine = create_engine(URL_DATA)
 
 # declarative base
 Base = declarative_base()
+
+Session = sessionmaker(engine)
+
+
+def db_session(func):
+    def wrapper(*args, **kargs):
+        session = Session()
+
+        try:
+            result = func(session, *args, **kargs)
+            session.commit()
+            return result
+        except Exception as e:
+            print(e)
+        finally:
+            session.close()
+    return wrapper
