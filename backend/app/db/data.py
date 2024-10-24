5c84939c84a30594bf42ed009b460a1ec4b5bebb
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-10-24 12:21:53 -0500

    fix: update declarative_base, .db extension

--- backend/app/db/data.py
+++ backend/app/db/data.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-URL_DATA = "sqlite:///./data"
+URL_DATA = "sqlite:///./data.db"
 engine = create_engine(URL_DATA)
 
 # declarative base
 Base = declarative_base()
-
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
