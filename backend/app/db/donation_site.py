357e9fb29d647c723fdd982717156e3594a3dcc9
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-10-24 02:15:03 -0500

    feat: add donation site table

---
+++ backend/app/db/donation_site.py
@@ -0,0 +1,10 @@
+from data import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+class DonationSide(Base):
+
+    __tablename__ = "DonationSide"
+    
+    id = Column(Integer, primary_key=True) 
+    bank = Column(Integer, ForeignKey('Bank.id'))
+    direction = Column(String)
