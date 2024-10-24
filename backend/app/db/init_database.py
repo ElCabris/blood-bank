f848fde360894a16206fbc0844ab65ed07e4a694
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-10-24 12:24:36 -0500

    feat: A script is added to start the database

---
+++ backend/app/db/init_database.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    from .data import Base, engine
+    from .tables.bank import Bank  # pylint: disable=W0611
+    from .tables.certificate import Certificate  # pylint: disable=W0611
+    from .tables.donation import Donation  # pylint: disable=W0611
+    from .tables.donation_site import DonationSite  # pylint: disable=W0611
+    from .tables.donor import Donor  # pylint: disable=W0611
+    from .tables.lot_blood import LoteBlood  # pylint: disable=W0611
+    from .tables.medical_history import MedicalHistory  # pylint: disable=W0611
+
+    Base.metadata.create_all(engine)
