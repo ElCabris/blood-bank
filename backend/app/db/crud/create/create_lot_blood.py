5c3d637a7d3c623e785941abc5db5ca30a4e9676
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-10-28 07:30:33 -0500

    refactor: formarter in create_lot_blood

--- backend/app/db/crud/create/create_lot_blood.py
+++ backend/app/db/crud/create/create_lot_blood.py
@@ -1,27 +1,35 @@
-from typing import Optional
 from datetime import date as Date
+from typing import Optional
 
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from app.db.data import engine
-from app.db.tables.lot_blood import LotBlood
 from app.db.tables.bank import Bank
 from app.db.tables.donor import Donor
+from app.db.tables.lot_blood import LotBlood
 
 
-def add_lot_blood(date: Date, liters: float, bank_name: str, donor: int) -> Optional[int]:
+def add_lot_blood(
+    date: Date, liters: float, bank_name: str, donor: int
+) -> Optional[int]:
 
     with Session(engine) as session:
-        query = session.query(Bank).add_column(Bank.id).filter(Bank.name == bank_name).first()
+        query = (
+            session.query(Bank)
+            .add_column(Bank.id)
+            .filter(Bank.name == bank_name)
+            .first()
+        )
 
         if query:
             bank_id = int(query.id)
         else:
             raise ValueError("not found bank")
 
-
-        query = session.query(Donor).add_column(Bank.id).filter(Donor.id == donor).first()
+        query = (
+            session.query(Donor).add_column(Bank.id).filter(Donor.id == donor).first()
+        )
 
         if query:
             donor_id = int(query.id)
@@ -30,7 +38,9 @@ def add_lot_blood(date: Date, liters: float, bank_name: str, donor: int) -> Opti
 
     with Session(engine) as session:
         try:
-            lot_blood = LotBlood(date=date, liters=liters, bank_id=bank_id, donor_id=donor_id)
+            lot_blood = LotBlood(
+                date=date, liters=liters, bank_id=bank_id, donor_id=donor_id
+            )
             session.add(lot_blood)
             session.commit()
             print(f"Lot blood {lot_blood} added sucessfully")
