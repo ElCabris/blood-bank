34488a8dd11bb8c7a3e99727653efc17f4f1e7a5
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-10-28 07:22:27 -0500

    fix: chache Bank to Donor

--- backend/app/db/crud/read/read_donor.py
+++ backend/app/db/crud/read/read_donor.py
@@ -2,16 +2,17 @@ from typing import Optional
 
 from sqlalchemy.orm import Session
 
-from app.db.tables.bank import Bank
 from app.db.data import engine
+from app.db.tables.donor import Donor
 
-def get_donor(id: int) -> Optional[Bank]:
+
+def get_donor(donor_id: int) -> Optional[Donor]:
 
     with Session(engine) as session:
 
-        query = session.query(Bank).filter(Bank.id == id).first()
+        query = session.query(Donor).filter(Donor.id == donor_id).first()
 
-        if query:
+        if query is not None:
             return query
         else:
             return None
