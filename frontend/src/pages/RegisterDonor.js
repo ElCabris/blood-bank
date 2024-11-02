808574ae1ef97ab09697d7e2c0fb4383d8db4b5c
David <david.lopez.rivera.2003@gmail.com>
2024-11-02 18:17:39 -0500

    Style: update interface login

--- frontend/src/pages/RegisterDonor.js
+++ frontend/src/pages/RegisterDonor.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import InputData from "../components/InputData";
+import './Login_2.css';
 
 function RegisterDonor() {
 	const [uploadMedicalHistory, setUploadMedicalHistory] = useState(null);
