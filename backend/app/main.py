8ac193ae1e1cfc7448c747f7e3fd174bb2800639
Emanuel Cabrera Novoa <133041097+ElCabris@users.noreply.github.com>
2024-11-04 02:44:48 -0500

    feat: CORS configuration (#94)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.bank import bank_router
 from app.api.v1.donor import donor_router
@@ -7,6 +8,14 @@ from app.db.init_database import init_db
 init_db()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(donor_router)
 app.include_router(bank_router)
